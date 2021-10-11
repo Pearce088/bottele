681eb0cd434825725510bb8adc6f1bb4a596b87f
Pearce088 <91814780+Pearce088@users.noreply.github.com>
2021-10-11 19:26:31 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,26 +36,7 @@ def file_sent(message):
                     bot.send_message(message.chat.id, short(bot.get_file_url(message.video[0].file_id)))
                 except AttributeError:
                     pass
-                
-@bot.message_handler(commands=['help'])
-def help_command(message):
-   keyboard = telebot.types.InlineKeyboardMarkup()
-   keyboard.add(
-       telebot.types.InlineKeyboardButton(
-           'Message the developer', url='telegram.me/aanmhd08'
-       )
-   )
-   bot.send_message(
-       message.chat.id,
-       '1) To receive a list of available currencies press /exchange.\n' +
-       '2) Click on the currency you are interested in.\n' +
-       '3) You will receive a message containing information regarding the source and the target currencies, ' +
-       'buying rates and selling rates.\n' +
-       '4) Click “Update” to receive the current information regarding the request. ' +
-       'The bot will also show the difference between the previous and the current exchange rates.\n' +
-       '5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.',
-       reply_markup=keyboard
-   )
+       
 
 
 while True:
