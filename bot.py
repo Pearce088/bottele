e883955afc0db70a4107ab6f7a5d147e05f7ef9e
Pearce088 <91814780+Pearce088@users.noreply.github.com>
2021-10-11 19:29:33 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,11 +16,27 @@ def send_welcome(message):
 @bot.message_handler(commands=['helpp'])
 def send_welcome(message):
     bot.reply_to(message, '- /shortener Untuk merubah file menjadi link.')
-
-@bot.message_handler(func=lambda message: True)
-def handle_all_message(message):
-    bot.reply_to(message, message.text)
     
+@bot.message_handler(commands=['help'])
+def help_command(message):
+   keyboard = telebot.types.InlineKeyboardMarkup()
+   keyboard.add(
+       telebot.types.InlineKeyboardButton(
+           ‘Message the developer’, url='telegram.me/artiomtb'
+       )
+   )
+   bot.send_message(
+       message.chat.id,
+       '1) To receive a list of available currencies press /exchange.\n' +
+       '2) Click on the currency you are interested in.\n' +
+       '3) You will receive a message containing information regarding the source and the target currencies, ' +
+       'buying rates and selling rates.\n' +
+       '4) Click “Update” to receive the current information regarding the request. ' +
+       'The bot will also show the difference between the previous and the current exchange rates.\n' +
+       '5) The bot supports inline. Type @<botusername> in any chat and the first letters of a currency.',
+       reply_markup=keyboard
+   )
+
 @bot.message_handler(content_types=['photo', 'video', 'audio', 'document'])
 def file_sent(message):
     try:
